0ec8fd10eb6acb5c44ec8ee28f2fd49786c5cff7
myusername <o.yangol@g.nsu.ru>
2020-05-02 21:54:32 +0700

    query

--- src/main/java/database/app_database/Dao/EmployeeDao.java
+++ src/main/java/database/app_database/Dao/EmployeeDao.java
@@ -1,10 +1,21 @@
 package database.app_database.Dao;
 import database.app_database.Model.Employee.Employee;
+import database.app_database.Model.Employee.QEmployee;
 import org.springframework.stereotype.Repository;
+import static database.app_database.Model.Employee.QEmployee.employee;
+import java.util.List;
 
 @Repository
-public class EmployeeDao  {
+public class EmployeeDao extends BaseEntityDao<Employee, QEmployee> {
+    public EmployeeDao() {
+        super(employee);
+    }
 
+    public List<Employee> getAll() {
+        return from(employee)
+                .select(employee)
+                .fetch();
+    }
 }
 
 
